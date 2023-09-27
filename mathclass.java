5526278f860ad18b1899419a0f61db189322f0e1
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2023-09-27 10:29:40 +0530

    Absolute Values

---
+++ mathclass.java
@@ -0,0 +1,9 @@
+public class mathclass {
+    public static void main (String args []){
+        double pi= 3.14;
+        double y = -10;
+        double result = Math.abs(y);
+        System.out.println(result);
+
+    }
+}
