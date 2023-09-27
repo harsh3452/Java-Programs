79efbae12449a5cb15214bf72d678510ef051cd5
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2023-09-27 09:42:45 +0530

    Using Expression in java

---
+++ exp.java
@@ -0,0 +1,11 @@
+public class exp {
+    public static void main (String []args){
+        int friends= 10;
+        System.out.println(friends++);
+        System.out.println(friends--);
+        System.out.println(++friends);
+        double result = (double)friends / 3;
+        System.out.println(result);
+        
+    }
+}
