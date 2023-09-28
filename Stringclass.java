badd2332eaa84dffe58d6ad0a69dc7559b725cdd
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2023-09-28 11:44:36 +0530

    Fuctions of String Methods

---
+++ Stringclass.java
@@ -0,0 +1,16 @@
+public class Stringclass {
+    public static void main (String args []){
+        String name = "   harsh    ";
+       // boolean result = name.equalsIgnoreCase("harsh");
+       //int result = name.length();
+       //char result = name.charAt(3);
+       //int result = name.indexOf("r");
+      // boolean result = name.isEmpty();
+      //String result = name.toUpperCase();
+     // String result = name.toLowerCase();
+     //String result = name.trim();
+        String result = name.replace('h','p');
+        System.out.println(result);
+
+    }
+}
