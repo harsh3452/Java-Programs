bb998ffca732832539d7ed13f89db057a2e1ceda
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2023-09-29 00:07:01 +0530

    Final Keyword in Java

---
+++ finalkeyword.java
@@ -0,0 +1,7 @@
+public class finalkeyword {
+    public static void main (String args []){
+        final double num = 3.14;
+        //num = 43.13; this cannot be assigned because final keyword is used 
+        System.out.println(num);
+    }
+}
