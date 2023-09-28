df413a2aaedd85521bd15da6bff61b2c04f629ae
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2023-09-28 17:53:15 +0530

    2D ArraysLists in Java

---
+++ TwoDimensionalArrayList.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class TwoDimensionalArrayList {
+    public static void main (String args []){
+        //2D ArrayLists = Dynamic list of lists. Also can change size during runtime
+
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+        ArrayList<String> bakeryList = new ArrayList();
+        bakeryList.add("baking powder");
+        bakeryList.add("cream");
+        bakeryList.add("Cheese");
+        bakeryList.add("Donuts");
+
+        ArrayList<String> produceList = new ArrayList();
+        produceList.add("Onions");
+        produceList.add("tomatoes");
+        produceList.add("Shinigami love Apples for the human world!");
+        produceList.add("Ground Nuts");
+        ArrayList<String> drinkList = new ArrayList();
+        drinkList.add("Soda");
+        drinkList.add("Sauce(of what?huh?)");
+        drinkList.add("Apple Juice!");
+        drinkList.add("Beer");
+
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(drinkList);
+
+        System.out.println(groceryList.get(1).get(0));
+    }
+}
