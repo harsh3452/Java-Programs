1d34b54ab28c61d7ef6e4efd8288d0b85bf35709
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2023-09-28 17:51:44 +0530

    Addition Methods

---
+++ addmethods.java
@@ -0,0 +1,12 @@
+public class addmethods {
+    public static void main (String args []){
+        int x = 78;
+        int y = 43;
+      //  int result = 
+      add(x,y);
+       // System.out.println("result");
+    }
+    static void add(int a, int b){
+        System.out.println(a+b);
+    }
+}
