272feec4fc0276e5b0d65c8594452db5b4c34c7d
harsh3452 <harshpatil3452@gmail.com>
2023-09-30 09:44:38 -0700

    Constructors  in Java

--- constructors.java
+++ constructors.java
@@ -2,12 +2,15 @@ public class constructors {
     public static void main (String args []){
         Human human1 = new Human("Rick",63,56);
         Human human2 = new Human("Morty",43,34.34);
+        Human human3 = new Human("Aaryan",432,345.34);
         System.out.println(human1.name);
         System.out.println(human2.name);
         human1.eat();
         human2.eat();
+        human3.eat();
         human1.drink();
         human2.drink();
+        human3.drink();
     }
 }
 
