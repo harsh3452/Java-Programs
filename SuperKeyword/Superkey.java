d550d2a70b0b45b7406f18c1dfa82cea5b70d0da
harsh3452 <harshpatil3452@gmail.com>
2023-10-02 09:23:25 +0530

    Super Keyword

---
+++ SuperKeyword/Superkey.java
@@ -0,0 +1,16 @@
+package SuperKeyword;
+
+public class Superkey {
+    public static void main(String args []){
+    //super = keyword refers to superclass (parent) of an object 
+    // very similar to the "this keyword"
+        Hero hero1 = new Hero("Batman",42,"$$$");
+        Hero hero2 = new Hero("Superman", 45, "Everything");
+        System.out.println(hero1.name);
+        System.out.println(hero1.age);
+        System.out.println(hero1.power);
+        System.out.println(hero2.toString());
+
+    }
+    
+}
